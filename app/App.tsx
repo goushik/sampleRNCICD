584997e56c0f8d2dd02554c70898b3a2e3230bb4
UdayKiran Samineni <udaykiransamineni@UdayKiran-ka-MacBook-Air.local>
2020-06-08 12:10:33 +0530

    initial commit

---
+++ app/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+     <View>
+     <Text style={{marginTop:'50%',textAlign:'center'}}>Sample RNCICD </Text>
+     </View>
+  );
+};
+
+export default App;
+
